5975024308f154a4bae725e5b4a3ba9c17d391ed
Raghav Rastogi <raghavrastogi594@gmail.com>
2023-07-07 10:42:41 +0530

    update sha256 of binary

--- nimbus.rb
+++ nimbus.rb
@@ -2,7 +2,7 @@ class Nimbus < Formula
   desc "CLI based tool to get insights on rules/alerts applied, based on Victoria Metrics"
   homepage "https://github.com/rag594/nimbus"
   url "https://github.com/rag594/nimbus/releases/download/v0.0.4/nimbus-mac-amd64.tar.gz"
-  sha256 "94bbe773617025e3bc787c1ffcda57492fc1b68f57a03b5d8d647949c7f242f3"
+  sha256 "6e457ebc0e9d6af5c7bdaf22fefc31eacc139eca8dbec2fb9af94b8d1e0c5fad"
   version "0.0.4"
   license "MIT"
 
