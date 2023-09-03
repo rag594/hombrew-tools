efb11556835593741fda09f0021efc4ef49efb8b
Raghav Rastogi <raghavrastogi594@gmail.com>
2023-09-03 18:02:33 +0530

    update nimbus shasum

--- nimbus.rb
+++ nimbus.rb
@@ -2,7 +2,7 @@ class Nimbus < Formula
   desc "CLI based tool to get insights on rules/alerts applied, based on Victoria Metrics"
   homepage "https://github.com/rag594/nimbus"
   url "https://github.com/rag594/nimbus/releases/download/v1.0.0/nimbus-mac-amd64.tar.gz"
-  sha256 "d651b7359fc3e2d77d6cfda3f6df616d21b44855665ff91e4a03c3496dc4f297"
+  sha256 "b8682ed8f4f68b9aea65875db2561c974455109d3e33bea46bce3da2c61c4a83"
   version "1.0.0"
   license "MIT"
 
